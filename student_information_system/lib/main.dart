7c49a31fe3b270d2a40d490a6c4908a8b54578ec
emineinan <emineinan@users.noreply.github.com>
2020-09-09 17:01:58 +0300

    student class is added.

--- student_information_system/lib/main.dart
+++ student_information_system/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:student_information_system/models/student.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -7,31 +8,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  List<Student> students = [
+    Student("Ali", "Aydın", 45,
+        "https://cdn.pixabay.com/photo/2014/09/17/11/47/man-449406__340.jpg"),
+    Student("Ayse", "Yılmaz", 90,
+        "https://cdn.pixabay.com/photo/2016/10/19/14/03/model-1753032__340.jpg")
+  ];
   @override
   Widget build(BuildContext context) {
-    var students = ["Ali Yılmaz", "Ayse Sahin", "Ceren Uslu"];
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Student Information System"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-                child: ListView.builder(
-                    itemCount: students.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Text(students[index]);
-                    })),
-            Center(
-              child: RaisedButton(
-                  child: Text("Click for Exam Result"),
-                  onPressed: () {
-                    var message = displayExamResult(30);
-                    displayMessage(context, message);
-                  }),
-            ),
-          ],
-        ));
+      appBar: AppBar(
+        title: Text("Student Information System"),
+      ),
+      body: buildBody(context),
+    );
   }
 
   String displayExamResult(int score) {
@@ -46,9 +36,53 @@ class MyApp extends StatelessWidget {
 
   void displayMessage(BuildContext context, String message) {
     var alert = AlertDialog(
-      title: Text("Exam Result"),
+      title: Text("Exam Result!"),
       content: Text(message),
     );
     showDialog(context: context, builder: (BuildContext context) => alert);
+  }
+
+  Widget buildBody(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+            child: ListView.builder(
+                itemCount: students.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(students[index].url),
+                    ),
+                    title: Text(students[index].firstName +
+                        " " +
+                        students[index].lastname),
+                    subtitle:
+                        Text("Exam grade: " + students[index].grade.toString()),
+                    trailing: buildStatusIcon(students[index].grade),
+                    onTap: () {
+                      print(students[index].firstName +
+                          " " +
+                          students[index].lastname);
+                    },
+                  );
+                })),
+        Center(
+          child: RaisedButton(
+              child: Text("Click for Exam Result"),
+              onPressed: () {
+                var message = displayExamResult(30);
+                displayMessage(context, message);
+              }),
+        ),
+      ],
+    );
+  }
+
+  Widget buildStatusIcon(int grade) {
+    if (grade >= 50) {
+      return Icon(Icons.done);
+    } else {
+      return Icon(Icons.clear);
+    }
   }
 }
---
+++ student_information_system/lib/models/student.dart
@@ -0,0 +1,15 @@
+class Student {
+  String firstName;
+  String lastname;
+  int grade;
+  String url;
+  String status;
+
+  Student(String firstName, String lastName, int grade, String url) {
+    this.firstName = firstName;
+    this.lastname = lastName;
+    this.grade = grade;
+    this.url = url;
+    this.status = "Passed";
+  }
+}
